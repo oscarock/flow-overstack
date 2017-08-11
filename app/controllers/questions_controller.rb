a3ccbc597d8413f2308d6941a67e7fe901edb537
Oscar Diaz <o-pipe@hotmail.com>
2017-08-10 19:43:31 -0500

    Crea la funcionalidad de crear Questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,27 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to questions_path
+    else
+      @errors = @question.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :content).merge(user_id: current_user.id)
+    end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,10 @@
 class Question < ApplicationRecord
+  #validations
+  validates :title, presence: true
+  validates :content, presence: true
+  #relations
   belongs_to :user
   has_many :comments, as: :commentable
   has_many :votes, as: :voteable
+
 end
